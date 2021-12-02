6d1ea7d20e1bc9fd885970b8c471ba639b1b26dd
RouxRhett <nsch.4871@gmail.com>
2021-12-02 22:00:06 +0900

    [change]どの画面からもbookの新規投稿を可能にし、他人の情報を編集できない様にした、レイアウトも一部変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:update,:edit]
 
   def show
     @user = User.find(params[:id])
@@ -18,9 +18,10 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      @books = @user.books
+      render "edit"
     end
   end
 
@@ -32,6 +33,7 @@ class UsersController < ApplicationController
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
+      flash[:notice] = "Not authorized!!"
       redirect_to user_path(current_user)
     end
   end
