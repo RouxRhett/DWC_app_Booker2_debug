87c37634ef2e2b176b6f67c848c421df829033ec
RouxRhett <nsch.4871@gmail.com>
2021-12-07 17:55:39 +0900

    [add]いいね用のモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users #これを一番最初に書かないと、deviseじゃない方のusers#アクションが呼ばれてしまう。
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   root "homes#top"
   get "home/about" => "homes#about"
 end
