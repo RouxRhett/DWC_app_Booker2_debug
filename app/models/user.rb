3918264090aeb80c0912ed523cbdf8b8643db080
RouxRhett <nsch.4871@gmail.com>
2021-12-08 18:35:57 +0900

    [add]フォロー機能用の関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,17 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
-  
+  #relationshipモデルを指定する。外部キーによって差別化
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent:   :destroy
+  #followed_id用の関連付け。followedsにならない様に名前を変更
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
