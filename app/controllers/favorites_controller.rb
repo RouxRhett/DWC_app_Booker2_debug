3871db5431737b1e8dfd30eb9133a81dd598d3a2
RouxRhett <nsch.4871@gmail.com>
2021-12-19 23:39:22 +0900

    [add]jsファイルの作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,15 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_back(fallback_location:root_path)
+    #リダイレクト先を削除する
+    #redirect_back(fallback_location:root_path)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_back(fallback_location:root_path)
+    #redirect_back(fallback_location:root_path)
   end
 
   #特定の操作によって「ActionController::InvalidAuthenticityToken」が発生する。課題終了後要確認
