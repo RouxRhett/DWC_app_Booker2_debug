3428a23a6e3f1e64996e1b22f249d78b268969d7
RouxRhett <nsch.4871@gmail.com>
2021-12-20 23:44:41 +0900

    [change]コメント機能の非同期通信化及びコントローラー名の一部変更

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,16 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location:root_path)
+    #redirect_back(fallback_location:root_path) 
   end
 
   def destroy
     PostComment.find_by(id: params[:id]).destroy
-    redirect_back(fallback_location:root_path)
+    @book = Book.find(params[:book_id])
+    #redirect_back(fallback_location:root_path)
   end
 
   def post_comment_params
